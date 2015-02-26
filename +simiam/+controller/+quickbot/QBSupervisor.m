91c719be062e2c96bf77f7f7974b0298697c90ff
nhreis <nreis@ucdavis.edu>
2015-02-25 18:45:18 -0800

    redefine goal
    per jeff suggestion:
     instead of making the desired coordinates negative try adding 180 deg
    to the goal before the final translation. Or mirror it about clocky. By
    saying negitive it's the same as mirroring it around (0,0).

--- +simiam/+controller/+quickbot/QBSupervisor.m
+++ +simiam/+controller/+quickbot/QBSupervisor.m
@@ -137,7 +137,7 @@ classdef QBSupervisor < simiam.controller.Supervisor
             inputs.d_fw = obj.d_fw;
             inputs.x_g = obj.goal(1)
             inputs.y_g = obj.goal(2)
-            
+            putvar(obj);
             %testing
 %             obj.robot.get_danger()
             
@@ -147,6 +147,7 @@ classdef QBSupervisor < simiam.controller.Supervisor
             camera_distances = obj.robot.get_camera_distances();
             
             dangers = obj.robot.get_danger();    %returns array of bool from ea camera
+            putvar(dangers);
             %dangers = [0;1;0;0;0];
             
             if (obj.check_event('at_goal') && ~any(dangers)) %at goal and safe
@@ -492,7 +493,7 @@ classdef QBSupervisor < simiam.controller.Supervisor
                 R = obj.get_transformation_matrix(x_s,y_s,theta_s);
                 dangers_rf(:,i) = R*[dangers(i); 0; dangers(i)];
             end
-            
+            putvar(dangers_rf);
             % 2. Apply the transformation to world frame.
             
             [x,y,theta] = state_estimate.unpack();
@@ -501,22 +502,23 @@ classdef QBSupervisor < simiam.controller.Supervisor
             dangers_wf = R*dangers_rf;
             
             dangers_wf = dangers_wf(1:2,:);    %just x,y
-            
+            putvar(dangers_wf);
             %create run away vector
             u_i = (dangers_wf-repmat([x;y],1,nSensors));
             u_dangers = sum(u_i,2);
-            
-            theta = atan2(u_dangers(2),u_dangers(1));
-            
+            putvar(u_dangers);
+            theta = atan2(u_dangers(2),u_dangers(1))+(180*pi/180);
+            putvar(theta);
             %set carrot for runaway
             x_n = x+0.25*cos(theta);
             y_n = y+0.25*sin(theta);
-            
+            putvar(x_n);
+            putvar(y_n);
             %assign to the inputs array variables 
-            inputs.x_g = -x_n;
-            obj.goal(1) = -x_n;
-            inputs.y_g = -y_n;
-            obj.goal(2) = -y_n;
+            inputs.x_g = x_n;
+            obj.goal(1) = x_n;
+            inputs.y_g = y_n;
+            obj.goal(2) = y_n;
         end
         
         %Create Transformation matrix for moving between WorldFrame and
